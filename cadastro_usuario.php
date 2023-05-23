fd8d645ee6276077b72d2ecf5c7b3c504131024a
GabrielGroppo <gabrielmgruppogg@gmail.com>
2023-05-23 10:50:24 -0300

    msg

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -7,7 +7,20 @@
     $telefone = $_POST['telefone'];
     $senha = $_POST['senha'];
 
+    $compara = "SELECT * FROM usuario WHERE email_usuario = '$email'";
+    $result = mysqli_query($con, $compara);
+    $row = mysqli_num_rows($result);
 
+    if ($row > 0){
+        echo "<h1>Usuário já cadastrado!</h1>";
+        echo "<a href='cadastro_usuario.html'>Voltar</a>";
+        exit();
+    }
+    else{
+        $sql = "INSERT INTO usuario (nome_usuario, email_usuario, fone_usuario, senha)";
+        $sql .= " VALUES ('".$nome."', '".$email."', '".$telefone."', '".$senha."')";
+
+    }
     
 
     echo "<h3>Dados do Usuário";
